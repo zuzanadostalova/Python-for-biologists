11cd772067947948ac93c0a1b08036d227860bf2
Zuzana Dostálová <zuzanadostalova@dhcp-72-025.eduroam.muni.cz>
2019-12-13 15:46:43 +0100

    ..

--- Print.py
+++ Print.py
@@ -1,5 +1,9 @@
 # Chapter 2 - Printing and manipulating text
+# new GitHub repository -> username -> repositories -> new-name -> 
+# create file on GitHub -> open upper terminal with cmd + shift + P, Git: clone -> 
+# coppied URL from GitHub -> create through it file on the laptop
+
 # do not omit quotes
 # use comments
-# error messages and debugging
-nnn,,
+# error messages and debugging - common buggs (?): forgotten quotes, 
+# mis-spelling, statement splitted over two lines
