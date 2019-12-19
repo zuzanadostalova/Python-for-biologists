4f880c192b15e2acdd948fa419246315db319feb
Zuzana Dostálová <zuzanadostalova@Zuzana-Air.homenet.telecomitalia.it>
2019-12-19 13:07:01 +0100

    ..

--- Reading_writing.py
+++ Reading_writing.py
@@ -45,12 +45,23 @@ seq_1 = "ATCGTACGATCGATCGATCGCTAGACGTATCG"
 seq_2 = "actgatcgacgatcgatcgatcacgact"
 seq_3 = "ACTGAC-ACTGT--ACTGTA----CATGTG"
 
-# make a new file to hold the output
-output = open("sequences.fasta", "w")
+# # make a new file to hold the output
+# output = open("sequences.fasta", "w")
 
-# write the header and sequence for seq1
-output.write(">" + header_1 + "\n" + seq_1 + "\n")
-# write the header and uppercase sequences for seq2
-output.write(">" + header_2 + "\n" + seq_2.upper() + "\n")
-# write the header and sequence for seq3 with hyphens removed
-output.write(">" + header_3 + "\n" + seq_3.replace("-","") + "\n")
+# # write the header and sequence for seq1
+# output.write(">" + header_1 + "\n" + seq_1 + "\n")
+# # write the header and uppercase sequences for seq2
+# output.write(">" + header_2 + "\n" + seq_2.upper() + "\n")
+# # write the header and sequence for seq3 with hyphens removed
+# output.write(">" + header_3 + "\n" + seq_3.replace("-","") + "\n")
+
+# Writing multiple fasta files
+# three new files to hold the output using concatenation
+output_1 = open(header_1 + ".fasta", "w")
+output_2 = open(header_2 + ".fasta", "w")
+output_3 = open(header_3 + ".fasta", "w")
+
+# one sequence to each output file
+output_1.write(">" + header_1 + "\n" + seq_1 + "\n")
+output_2.write(">" + header_2 + "\n" + seq_2.upper() + "\n")
+output_3.write(">" + header_3 + "\n" + seq_3.replace("-", "") + "\n")
