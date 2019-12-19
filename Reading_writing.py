0857885cf967e7446f5844de7c830b9763323ee2
Zuzana Dostálová <zuzanadostalova@Zuzana-Air.homenet.telecomitalia.it>
2019-12-19 12:31:32 +0100

    ...

--- Reading_writing.py
+++ Reading_writing.py
@@ -12,11 +12,14 @@
 
 # Exercises
 
-# Splitting genomic DNA
+# 1. Splitting genomic DNA
 my_DNA_file = open("GenomicDNA.txt") 
+# .txt - just a plain text, no variables, just the DNA sequence
+# always save also text files or you will not see the output
 my_DNA = my_DNA_file.read()
+
 exon_1 = my_DNA[0:63]
-non_coding = my_DNA[63:90]
+intron = my_DNA[63:90]
 exon_2 = my_DNA[90: ]
 # print(exon_1.upper() + non_coding.lower() + exon_2.upper())
 
@@ -29,4 +32,25 @@ coding = open("Coding.txt", "w")
 coding.write(exon)
 
 noncoding = open("Non_coding.txt", "w")
-noncoding.write(non_coding.lower())
+noncoding.write(intron.lower())
+
+# 2. FASTA file
+# set the values of all the header variables
+header_1 = "ABC123"
+header_2 = "DEF456"
+header_3 = "HIJ789"
+
+# set the values of all the sequence variables
+seq_1 = "ATCGTACGATCGATCGATCGCTAGACGTATCG"
+seq_2 = "actgatcgacgatcgatcgatcacgact"
+seq_3 = "ACTGAC-ACTGT--ACTGTA----CATGTG"
+
+# make a new file to hold the output
+output = open("sequences.fasta", "w")
+
+# write the header and sequence for seq1
+output.write(">" + header_1 + "\n" + seq_1 + "\n")
+# write the header and uppercase sequences for seq2
+output.write(">" + header_2 + "\n" + seq_2.upper() + "\n")
+# write the header and sequence for seq3 with hyphens removed
+output.write(">" + header_3 + "\n" + seq_3.replace("-","") + "\n")
