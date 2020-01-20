7912d7a344a4b910cac4616bf4feff95c015ef07
Zuzana Dostálová <zuzanadostalova@dhcp-79-199.eduroam.muni.cz>
2020-01-20 14:28:39 +0100

    ...

--- Lists_loops.py
+++ Lists_loops.py
@@ -76,34 +76,97 @@
 # if only two print statements - easier changes
 # adding elements to the loop - we do not need to change the loop
 
-# Indentation errors
-apes = ["Homo sapiens", "Pan  troglodytes", "Gorilla gorilla"]
+# # Indentation errors
+# apes = ["Homo sapiens", "Pan  troglodytes", "Gorilla gorilla"]
 
-for ape in apes:
-    name_length = len(ape)
-   first_letter = ape[0]
-    print(ape + " is an ape. Its name starts with " + first_letter)
+# for ape in apes:
+#     name_length = len(ape)
+#    first_letter = ape[0]
+#     print(ape + " is an ape. Its name starts with " + first_letter)
 
-# because the lines of the code do not match up
-# only space or tab for indentation -> tab emulation
+# # because the lines of the code do not match up
+# # only space or tab for indentation -> tab emulation
 
-# Using a string as a list
-# string in the position of a list -  each character in the list as a separate element
+# # Using a string as a list
+# # string in the position of a list -  each character in the list as a separate element
 
-name = "python"
-for character in name:
-    print("One character is: " + character)
-# repeating a set of information for each element of the list = iteration
-# iteration over a list or a string
+# name = "python"
+# for character in name:
+#     print("One character is: " + character)
+# # repeating a set of information for each element of the list = iteration
+# # iteration over a list or a string
 
-# Splitting a string to make a list
-# split() takes a single argument called delimiter and splits the string 
-# where it sees the delimeter producing a list
-names = "melanogaster,simulans,yakuba,ananasse"
-species = name.split(",")
-print(str(species))
+# # Splitting a string to make a list
+# # split() takes a single argument called delimiter and splits the string 
+# # where it sees the delimeter producing a list
+# names = "melanogaster,simulans,yakuba,ananasse"
+# species = name.split(",")
+# print(str(species))
 
-# this created  list can be iterated over using a loop
+# # this created list can be iterated over using a loop
 
+# # Iterating over lines in a file
+# # both string and file can be treated as list and iterate over
+# # but in string each character becomes an individual element whereas in a file
+# # each line becomes an individual element
 
+# # iterating over file object not contents
+# file = open("some_input.txt")
+# for line in file:
 
+# # iterating over file contents
+# file = open("some_input.txt")
+
+# # print length of each line
+# for line in file:
+#     print("The length is " + str(len(line)))
+
+# # print the first character of each line
+# for line in file:
+#     print("The first character is " + line[0]) # this line never gets executed
+# # because file objects are exhaustible - it reads all the lines in the first for loop
+
+# # solution:
+# # I. reopen the file each time
+# # II. read the lines of the file into a list, iterate over list, .readlines() returns 
+# # a list of all the lines in the file
+
+# # store a list of lines in the file
+# file = open("some_input.txt")
+# all_lines = file.readlines()
+
+# # print the lengths
+# for line in all_lines:
+#     print("The length is " + str(len(line)))
+
+# # print the first characters
+# for line in all_lines:
+#     print("The first character is " + line[0])
+
+# # Loops with ranges
+# # iterate over a list of numbers
+
+protein = "vlspadktnv"
+print(protein[0:3]) # substring notation, without loop repetitive
+# Output: "vls"
+
+# solution - range() - generates lists of numbers (Python2), range object (Python3)
+# its behaviour depends on the number of the arguments
+
+# single argument - count up from 0 to that number
+for number in range(6):
+    print(number)
+# Output: from 0 to 5
+
+# two numbers
+for number in range(3, 7):
+    print(number)
+# Output: from 3 to 8
+
+# three numbers
+for number in range(2, 14, 4):
+    print(number)
+# Output: 2, 6, 10
+
+# lists - many elements in a single variable
+# loops - let us process these elements one by one
