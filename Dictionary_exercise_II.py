dde79c60d4132b87d13b461a904a05ae3ab0d59c
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2020-02-06 21:10:49 +0100

    ..

--- Dictionary_exercise_II.py
+++ Dictionary_exercise_II.py
@@ -418,48 +418,48 @@ dicti = {'students': [{'firstName': 'Nikki', 'lastName': 'Roysden'}, {'firstName
 'lastName': 'Friedland'}, {'firstName': 'Aron ', 'lastName': 'Wilkins'}], 'teachers': 
 [{'firstName': 'Amberly', 'lastName': 'Calico'}, {'firstName': 'Regine', 'lastName': 'Agtarap'}]}
 
-# Sol.I:
-import json
-with open('result.json', 'w') as fp:
-    json.dump(dicti, fp)   
+# # Sol.I:
+# import json
+# with open('result.json', 'w') as fp:
+#     json.dump(dicti, fp)   
 
-with open('result.json') as f:
-  data = json.load(f)
-print(data)
-# {'students': [{'firstName': 'Nikki', 'lastName': 'Roysden'}, {'firstName': 'Mervin', 
-# 'lastName': 'Friedland'}, {'firstName': 'Aron ', 'lastName': 'Wilkins'}], 'teachers': 
-# [{'firstName': 'Amberly', 'lastName': 'Calico'}, {'firstName': 'Regine', 'lastName': 'Agtarap'}]}
-print(type(data))
+# with open('result.json') as f:
+#   data = json.load(f)
+# print(data)
+# # {'students': [{'firstName': 'Nikki', 'lastName': 'Roysden'}, {'firstName': 'Mervin', 
+# # 'lastName': 'Friedland'}, {'firstName': 'Aron ', 'lastName': 'Wilkins'}], 'teachers': 
+# # [{'firstName': 'Amberly', 'lastName': 'Calico'}, {'firstName': 'Regine', 'lastName': 'Agtarap'}]}
+# # print(type(data))
 
-# Sol.II:
-d = {"students":[{"firstName": "Nikki", "lastName": "Roysden"},
-               {"firstName": "Mervin", "lastName": "Friedland"},
-               {"firstName": "Aron ", "lastName": "Wilkins"}],
-"teachers":[{"firstName": "Amberly", "lastName": "Calico"},
-         {"firstName": "Regine", "lastName": "Agtarap"}]}
-print("Original dictionary:")
-print(d)
-print(type(d))
-import json
+# # Sol.II:
+# d = {"students":[{"firstName": "Nikki", "lastName": "Roysden"},
+#                {"firstName": "Mervin", "lastName": "Friedland"},
+#                {"firstName": "Aron ", "lastName": "Wilkins"}],
+# "teachers":[{"firstName": "Amberly", "lastName": "Calico"},
+#          {"firstName": "Regine", "lastName": "Agtarap"}]}
+# print("Original dictionary:")
+# print(d)
+
+# import json
  
-with open("dictionary", "w") as f:
-   json.dump(d, f, indent = 4, sort_keys = True)
+# with open("dictionary", "w") as f:
+#    json.dump(d, f, indent = 4, sort_keys = True)
  
-print("\nJson file to dictionary:")
-with open('dictionary') as f:
- data = json.load(f)
-print(data)
+# print("\nJson file to dictionary:")
+# with open('dictionary') as f:
+#  data = json.load(f)
+# print(data)
 
-# 40. Write a Python program to create a dictionary of keys x, y, and z where each key has as 
-# value a list from 11-20, 21-30, and 31-40 respectively. Access the fifth value of each key from 
-# the dictionary.
+# # 40. Write a Python program to create a dictionary of keys x, y, and z where each key has as 
+# # value a list from 11-20, 21-30, and 31-40 respectively. Access the fifth value of each key from 
+# # the dictionary.
 {'x': [11, 12, 13, 14, 15, 16, 17, 18, 19],
 'y': [21, 22, 23, 24, 25, 26, 27, 28, 29],
 'z': [31, 32, 33, 34, 35, 36, 37, 38, 39]}
 
 from pprint import pprint
 dict_nums = dict(x=list(range(11, 20)), y=list(range(21, 30)), z=list(range(31, 40)))
-print(dict_nums)
+pprint(dict_nums)
 print(dict_nums["x"][4])
 print(dict_nums["y"][4])
 print(dict_nums["z"][4])
